6f07545afe741651bcd3545d00fc4c2c2836cf76
saikrishna8861 <101323123+saikrishna8861@users.noreply.github.com>
2022-09-06 00:02:40 +0530

    fixed navbar drop down

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,10 @@ import {
 } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
 const Navbar = () => {
   const router = useRouter();
   const Sideref = useRef();
@@ -29,6 +32,9 @@ const Navbar = () => {
       setLoggedIn(false);
     }
   }, []);
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
     <>
@@ -78,72 +84,8 @@ const Navbar = () => {
                       e.preventDefault();
                       setOpenMenu(openMenu ? false : true);
                     }}
-                  >
-                    <button
-                      type="submit"
-                      className="inline-flex w-full justify-center rounded-md border  px-4 py-2 text-sm font-medium text-gray-700 shadow-sm   focus:ring-offset-2 focus:ring-offset-gray-100"
-                      id="menu-button"
-                      aria-expanded="true"
-                      aria-haspopup="true"
-                    >
-                      Hi, Rohan
-                      <svg
-                        className="-mr-1 ml-2 h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                  </form>
+                  ></form>
                 </div>
-                {openMenu ? (
-                  <div
-                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="menu-button"
-                    tabIndex="-1"
-                  >
-                    <div className="py-1" role="none">
-                      <button
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabIndex="-1"
-                        id="menu-item-0"
-                      >
-                        Account
-                      </button>
-                      <button
-                        onClick={() => {
-                          try {
-                            localStorage.setItem("jwt", "");
-                            router.push("/login");
-                            router.reload();
-                          } catch (error) {
-                            console.log("error", error);
-                          } finally {
-                            // window.location.reload(false);
-                          }
-                        }}
-                        className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
-                        role="menuitem"
-                        tabIndex="-1"
-                        id="menu-item-3"
-                      >
-                        Logout
-                      </button>
-                    </div>
-                  </div>
-                ) : (
-                  ""
-                )}
               </div>
               <button
                 className="border-0 py-1 px-3 rounded mt-4 md:mt-0 text-black text-2xl absolute top-4 right-4 "
@@ -154,6 +96,76 @@ const Navbar = () => {
                 </div>
                 <BsFillCartFill />
               </button>
+              <Menu as="div" className="relative inline-block text-left">
+                <div className="mr-20">
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm focus:outline-none  ">
+                    Account
+                    <ChevronDownIcon
+                      className="-mr-1 ml-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            My Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+
+                      <form method="POST" action="#">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              type="submit"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "block w-full px-4 py-2 text-left text-sm"
+                              )}
+                              onClick={() => {
+                                try {
+                                  localStorage.setItem("jwt", "");
+                                  router.push("/login");
+                                  router.reload();
+                                } catch (error) {
+                                  console.log("error", error);
+                                } finally {
+                                  // window.location.reload(false);
+                                }
+                              }}
+                            >
+                              Sign out
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </form>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
               <button
                 className="border-0 py-1 px-3 rounded mt-4 md:mt-0 text-black text-2xl absolute top-4 right-4 "
                 // onClick={toggleCart}
